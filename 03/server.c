d899685076cb07756bb82f92bec65c254b673c8f
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2026-03-25 09:45:47 +0100

    simple udp server - isPalindrome

---
+++ 03/server.c
@@ -0,0 +1,125 @@
+// Szkielet serwera UDP/IPv4 używającego gniazdka bezpołączeniowego.
+
+#define _POSIX_C_SOURCE 200809L
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include <string.h>
+
+bool isValidPalindrome(const char *str) {
+    size_t len = strlen(str);
+    for (size_t i = 0; i < len / 2; ++i) {
+        if (tolower(str[i]) != tolower(str[len - 1 - i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(void)
+{
+    int sock;
+    int rc;         // "rc" to skrót słów "result code"
+    ssize_t cnt;    // na wyniki zwracane przez recvfrom() i sendto()
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        return 1;
+    }
+
+    int port = 2020;
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_addr = { .s_addr = htonl(INADDR_ANY) },
+        .sin_port = htons(port)
+    };
+
+    rc = bind(sock, (struct sockaddr *) & addr, sizeof(addr));
+    if (rc == -1) {
+        perror("bind");
+        return 1;
+    }
+    printf("Server is listening on port: %d\n", port);
+
+    bool keep_on_handling_clients = true;
+    while (keep_on_handling_clients) {
+
+        unsigned char buf[1024];
+        struct sockaddr_in clnt_addr;
+        socklen_t clnt_addr_len;
+
+        clnt_addr_len = sizeof(clnt_addr);
+        cnt = recvfrom(sock, buf, sizeof(buf), 0,
+                (struct sockaddr *) & clnt_addr, & clnt_addr_len);
+        if (cnt == -1) {
+            perror("recvfrom");
+            return 1;
+        }
+        printf("received %zi bytes\n", cnt);
+        
+        buf[cnt] = '\0';
+        while (cnt > 0 && (buf[cnt - 1] == '\n' || buf[cnt - 1] == '\r')) {
+            cnt--;
+            buf[cnt] = '\0';
+        }
+
+        bool format_error = false;
+        if (buf[0] == ' ' || buf[cnt - 1] == ' ') {
+            format_error = true;
+        } else {
+            for (ssize_t i = 0; i < cnt - 1; i++) {
+                if (buf[i] == ' ' && buf[i + 1] == ' ') {
+                    format_error = true;
+                    break;
+                }
+            }
+        }
+        
+        char response[50];
+        if (format_error) {
+            snprintf(response, sizeof(response), "ERROR\n");
+        } else  {
+            char *words[100];
+            int word_count = 0;
+                char *word = strtok((char *)buf, " ");
+                while (word != NULL && word_count < 100) {
+                    words[word_count++] = word;
+                    word = strtok(NULL, " ");
+                }
+
+            int palindromeCounter = 0;
+            for (int i = 0; i < word_count; i++) {
+                bool isPalindrome = isValidPalindrome(words[i]);
+                if (isPalindrome) {
+                    palindromeCounter++;
+                }
+            }
+
+            snprintf(response, sizeof(response), "%d/%d\n", palindromeCounter, word_count);
+        }
+
+        cnt = sendto(sock, response, strlen(response), 0,
+                (struct sockaddr *) & clnt_addr, clnt_addr_len);
+        if (cnt == -1) {
+            perror("sendto");
+            return 1;
+        }
+        printf("sent %zi bytes\n", cnt);
+
+    }
+
+    rc = close(sock);
+    if (rc == -1) {
+        perror("close");
+        return 1;
+    }
+
+    return 0;
+}
