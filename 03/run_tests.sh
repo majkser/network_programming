3b43d2c9708a2c02a6880032e4784b563460e848
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2026-03-31 23:44:46 +0200

    added tests

---
+++ 03/run_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Uruchamianie testów..."
+
+for i in {1..17}; do
+    # Wysłanie danych do serwera i zapisanie wyniku
+    python3 mini-udpcat.py 127.0.0.1 2020 < "tests/test${i}-dane.txt" > "tests/wynik-test${i}.txt"
+    
+    # Porównanie wyniku (wymgamy dokładnej zgodności zawartości, beż żadnych odstępstw)
+    if cmp -s "tests/wynik-test${i}.txt" "tests/test${i}-wynik.txt"; then
+        echo "✅ Test $i: SUKCES"
+    else
+        echo "❌ Test $i: BŁĄD"
+        echo "  Oczekiwano: '$(cat tests/test${i}-wynik.txt)'"
+        echo "  Otrzymano : '$(cat tests/wynik-test${i}.txt)'"
+    fi
+done
+
+echo "-----------------------------------"
+
